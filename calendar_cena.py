3037293dc97a54bb9da9e9301a6fc9d3040387c6
shobanshone <106527322+shobanshone@users.noreply.github.com>
2022-11-15 11:39:14 +0530

    Update calendar_cena.py

--- calendar_cena.py
+++ calendar_cena.py
@@ -1,71 +1,40 @@
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-import pickle
-import pprint
-# from datetime import datetime, timedelta
-import datetime
 from google.auth.transport.requests import Request
 import os.path
+import pickle
 
 credentials=None
 
-if os.path.exists(r"D:\Shoban\programming\python\cena fin\token.pickle"):
+#Loading the credentials from pickle file
+if os.path.exists(r".\\token.pickle"):
                 print("Accessing credentials...")
                 with open("token.pickle",'rb') as file:
                     credentials= pickle.load(file)
 
-
+#if credentials are either expired or not exist , refresh or create credentials
 if not credentials or not credentials.valid:
     if credentials and credentials.expired and credentials.refresh_token:
-        try:
-            print("Refreshing access token....")
-            credentials.refresh(Request())
-        except:
-                print('not able to refresh token')
-                print("Fetching new access token....")
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    "client_secret.json", scopes=['https://www.googleapis.com/auth/calendar']
-                    )
-                flow.run_local_server(
+        print("Refreshing access token....")
+        credentials.refresh(Request())
+    else:
+        print('not able to refresh token')
+        print("Fetching new access token....")
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "client_secret.json", scopes=['https://www.googleapis.com/auth/calendar'])
+        flow.run_local_server(
                     port=8080, authorization_prompt_message=""
                     )
-                credentials = flow.credentials
-                with open("token.pickle",'wb') as file:
-                    print("Saving credentials for future use...")
-                    pickle.dump(credentials,file)
-# flow = InstalledAppFlow.from_client_secrets_file('client_secret.json',scopes=scopes)
-# credentials=flow.run_console()
-# credentials= '<google.oauth2.credentials.Credentials object at 0x0000021E38BE7F70>'
-# pickle.dump(credentials, open("token.pkl",'wb'))
-# credentials=pickle.load(open('token.pkl','rb'))
+        
+        credentials = flow.credentials
+        with open("token.pickle",'wb') as file:
+            print("Saving credentials for future use...")
+            pickle.dump(credentials,file)
 
-
-
-def create_event(start_time, client_name, number, calendarId):
-  
-    event = {
-        'summary': client_name,
-        'location': 'Thundukkadu, Tamilnadu',
-        'description': f'{client_name} - due {number}',
-        'start': {
-            'dateTime': start_time.strftime("%Y-%m-%dT08:00:%S"),
-            'timeZone': 'Asia/Kolkata',
-        },
-        'end': {
-            'dateTime': start_time.strftime("%Y-%m-%dT22:00:%S"),
-            'timeZone': 'Asia/Kolkata',
-        },
-        'reminders': {
-            'useDefault': False,
-            'overrides': [
-                {'method': 'email', 'minutes': 9 * 60},
-                {'method': 'popup', 'minutes': 10},
-            ],
-        },
-    }
-    print('ok')
-    # return remainder.events().insert(calendarId= calendarId, body=event).execute()
+#variable for storing calendar
 remainder=build('calendar', 'v3', credentials=credentials)
+
+#getting the calendar list and getting te id of the calendar 'fin'
 result=remainder.calendarList().list().execute()
 for calendar in result['items']:
                 if calendar['summary']== 'fin':
@@ -73,16 +42,16 @@ for calendar in result['items']:
 
 page_token = None
 
+#delete all events 
 while True:
     event=remainder.events().list(calendarId=calendarId, pageToken=page_token).execute()
-    # if(len(event['items'])==0):
-    #     print('all deleted')
-    #     break
+
     for index,items in enumerate(event['items']):
         remainder.events().delete(calendarId=calendarId,eventId= items['id'] ).execute()
         print(index)
     page_token = event.get('nextPageToken')
     print('iteration ended')
+    
     if not page_token:
         print('all deleted')
         break
